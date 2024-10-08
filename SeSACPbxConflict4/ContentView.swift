15c84b85cb0a09f9fb8be6f78528b3e8d86f58e4
Daeyun <sweet_ray@naver.com>
2024-10-08 14:10:46 +0900

    Initial Commit

---
+++ SeSACPbxConflict4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACPbxConflict4
+//
+//  Created by 권대윤 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
